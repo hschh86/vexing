ec78ff3347c9846931e60a91b6b7704f635d6326
xjh <hschh86@users.noreply.github.com>
2016-01-30 16:03:53 +1100

    reorganisation - create new files

---
+++ scripts/fsvg.js
@@ -0,0 +1,7 @@
+/*  fsvg.js: Adventures in SVG manipulation for fun and profit
+    ... okay, maybe just fun.                                  */
+
+var fsvg = (function() {
+  'use strict';
+
+}());    
